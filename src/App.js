36c03f3a65fc290679fd005b08bf435a82ab3f87
Ankit <ankitd349@gmail.com>
2023-05-14 15:46:50 +0530

    Add Basic Setup and UI part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button className="">Clear</button>
+          <button className="">C</button>
+          <button className="">&divide;</button>
+          <button className="">7</button>
+          <button className="">8</button>
+          <button className="">9</button>
+          <button className="">&times;</button>
+          <button className="">4</button>
+          <button className="">5</button>
+          <button className="">6</button>
+          <button className="">&ndash;</button>
+          <button className="">1</button>
+          <button className="">2</button>
+          <button className="">3</button>
+          <button className="">+</button>
+          <button className="">0</button>
+          <button className="">.</button>
+          <button className="">=</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
